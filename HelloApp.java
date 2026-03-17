95aa7f07110399631add96021a3ffb4bb4446753
jaindelisha06 <jaindelisha06@gmail.com>
2026-03-17 09:18:32 +0530

    uc-03-defaultname

--- HelloApp.java
+++ HelloApp.java
@@ -3,7 +3,12 @@ class HelloApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a name:");
-        String name = sc.next();
-        System.out.println("Hello, " + name + "!");
+        String name = sc.nextLine(); 
+        if (name.isEmpty()) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
+ 
